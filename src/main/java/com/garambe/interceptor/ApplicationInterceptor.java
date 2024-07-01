aff958cee1ae9b3ad7e4bcb40ecbb409ea461400
4876875 <algassim@hotmail.fr>
2024-07-01 11:54:49 +0200

    Intercepter les requetes HTTP

---
+++ src/main/java/com/garambe/interceptor/ApplicationInterceptor.java
@@ -0,0 +1,13 @@
+package com.garambe.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationInterceptor {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationInterceptor.class, args);
+	}
+
+}
